ffe6bf9d73131525e005011690ddb7e731bb9af2
Anna Özmehak <anna.ozmehak@gmail.com>
2024-05-28 14:55:49 +0200

    courseProvider done

---
+++ CourseProvider.Infrastructure/GraphQL/ObjectTypes/CourseType.cs
@@ -0,0 +1,63 @@
+﻿using CourseProvider.Infrastructure.Data.Entities;
+using CourseProvider.Infrastructure.Models;
+
+namespace CourseProvider.Infrastructure.GraphQL.ObjectTypes;
+
+public class CourseType : ObjectType<CourseEntity>
+{
+    protected override void Configure(IObjectTypeDescriptor<CourseEntity> descriptor)
+    {
+        descriptor.Field(c => c.Id).Type<NonNullType<IdType>>();
+        descriptor.Field(c => c.IsBestSeller).Type<BooleanType>();
+        descriptor.Field(c => c.IsDigital).Type<BooleanType>();
+        descriptor.Field(c => c.Categories).Type<ListType<StringType>>();
+        descriptor.Field(c => c.Title).Type<StringType>();
+        descriptor.Field(c => c.Ingress).Type<StringType>();
+        descriptor.Field(c => c.StarRating).Type<DecimalType>();
+        descriptor.Field(c => c.Reviews).Type<StringType>();
+        descriptor.Field(c => c.LikesInProcent).Type<StringType>();
+        descriptor.Field(c => c.Likes).Type<StringType>();
+        descriptor.Field(c => c.Hours).Type<StringType>();
+        descriptor.Field(c => c.Authors).Type<ListType<AuthorType>>();
+        descriptor.Field(c => c.Prices).Type<ListType<PricesType>>();
+        descriptor.Field(c => c.Content).Type<ListType<ContentType>>();
+    }
+}
+
+public class AuthorType : ObjectType<AuthorEntity>
+{
+    protected override void Configure(IObjectTypeDescriptor<AuthorEntity> descriptor)
+    {
+        descriptor.Field(a => a.Name).Type<StringType>();
+    }
+}
+
+public class PricesType : ObjectType<PricesEntity>
+{
+    protected override void Configure(IObjectTypeDescriptor<PricesEntity> descriptor)
+    {
+        descriptor.Field(a => a.Currency).Type<StringType>();
+        descriptor.Field(a => a.Price).Type<DecimalType>();
+        descriptor.Field(a => a.Discount).Type<DecimalType>();
+    }
+}
+
+public class ContentType : ObjectType<ContentEntity>
+{
+    protected override void Configure(IObjectTypeDescriptor<ContentEntity> descriptor)
+    {
+        descriptor.Field(a => a.Description).Type<StringType>();
+        descriptor.Field(a => a.Includes).Type<ListType<StringType>>();
+        descriptor.Field(a => a.ProgramDetails).Type<ListType<ProgramDetailItemType>>();
+    }
+}
+
+public class ProgramDetailItemType : ObjectType<ProgramDetailItemEntity>
+{
+    protected override void Configure(IObjectTypeDescriptor<ProgramDetailItemEntity> descriptor)
+    {
+        descriptor.Field(a => a.Id).Type<IntType>();
+        descriptor.Field(a => a.Title).Type<StringType>();
+        descriptor.Field(a => a.Description).Type<StringType>();
+    }
+}
---
+++ CourseProvider.Infrastructure/Services/CourseService.cs
@@ -0,0 +1,97 @@
+﻿using CourseProvider.Infrastructure.Data.Contexts;
+using CourseProvider.Infrastructure.Factories;
+using CourseProvider.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseProvider.Infrastructure.Services;
+
+public interface ICourseService
+{
+    Task<Course> CreateCourseAsync(CourseCreateRequest request);
+    Task<Course> GetCourseByIdAsync(string id);
+    Task<IEnumerable<Course>> GetCoursesAsync();
+    Task<Course> UpdateCourseAsync(CourseUpdateRequest request);
+    Task<bool> DeleteCourseAsync(string id);
+
+}
+public class CourseService(IDbContextFactory<DataContext> contextFactory) : ICourseService
+{
+    private readonly IDbContextFactory<DataContext> _contextFactory = contextFactory;
+
+
+    public async Task<Course> CreateCourseAsync(CourseCreateRequest request)
+    {
+        await using var context = _contextFactory.CreateDbContext();
+
+        var courseEntity = CourseFactory.Create(request);
+        context.Courses.Add(courseEntity);
+        await context.SaveChangesAsync();
+
+        return CourseFactory.Create(courseEntity);
+    }
+
+    public async Task<bool> DeleteCourseAsync(string id)
+    {
+        await using var context = _contextFactory.CreateDbContext();
+        var courseEntity = await context.Courses.FirstOrDefaultAsync(c => c.Id == id);
+        if (courseEntity == null) return false;
+
+        context.Courses.Remove(courseEntity);
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<Course> GetCourseByIdAsync(string id)
+    {
+        await using var context = _contextFactory.CreateDbContext();
+        var courseEntity = await context.Courses.FirstOrDefaultAsync(c => c.Id == id);
+
+        return courseEntity == null ? null! : CourseFactory.Create(courseEntity);
+    }
+
+    public async Task<IEnumerable<Course>> GetCoursesAsync()
+    {
+        await using var context = _contextFactory.CreateDbContext();
+        var courseEntities = await context.Courses.ToListAsync();
+
+        return courseEntities.Select(CourseFactory.Create);
+    }
+
+    public async Task<Course> UpdateCourseAsync(CourseUpdateRequest request)
+    {
+        await using var context = _contextFactory.CreateDbContext();
+        var existingCourse = await context.Courses
+            .Include(c => c.Authors)
+            .Include(c => c.Prices)
+            .Include(c => c.Content)
+            .ThenInclude(c => c.ProgramDetails)
+            .FirstOrDefaultAsync(c => c.Id == request.Id);
+
+        if (existingCourse == null) return null!;
+
+        var updatedCourseEntity = CourseFactory.Create(request);
+        updatedCourseEntity.Id = existingCourse.Id;
+
+        context.Entry(existingCourse).CurrentValues.SetValues(updatedCourseEntity);
+        
+        existingCourse.Content!.Description = updatedCourseEntity.Content!.Description;
+        existingCourse.Content.Includes = updatedCourseEntity.Content.Includes;
+        
+        existingCourse.Authors!.Clear();
+        foreach (var author in updatedCourseEntity.Authors!)
+        {
+            existingCourse.Authors.Add(author);
+        }
+
+        existingCourse.Content!.ProgramDetails!.Clear();
+        foreach (var programDetail in updatedCourseEntity.Content!.ProgramDetails!)
+        {
+            existingCourse.Content.ProgramDetails.Add(programDetail);
+        }
+
+        existingCourse.Prices = updatedCourseEntity.Prices;
+
+        await context.SaveChangesAsync();
+        return CourseFactory.Create(existingCourse);
+    }
+}
